35b49fa8721bfcd48b4dfdd547c116f1fe6a17c2
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2024-04-04 03:17:47 +0000

    Adds a sync script

---
+++ push-changes-to-etc-nixos.sh
@@ -0,0 +1,3 @@
+cp *.nix /etc/nixos/
+cp *.lock /etc/nixos
+cp -r containers /etc/nixos/containers
--- save-config-to-git.sh
+++
@@ -1,5 +0,0 @@
-cp /etc/nixos/*.nix $HOME/nixos-configuration
-cp /etc/nixos/*.lock $HOME/nixos-configuration
-rm $HOME/nixos-configuration/hardware-configuration.nix
-chown $USER:users *.nix
-chown $USER:users *.lock
